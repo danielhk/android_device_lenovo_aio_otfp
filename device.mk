4d300e41aa420db540b750615c66cc7e801228b2
daniel_hk <daniel.p6800@gmail.com>
2016-12-05 19:23:32 +0800

    Add media config files

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ TARGET_OTA_ASSERT_DEVICE := K50-t5,aio_otfp_m,K3Note
 # Audio policy & codec
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
