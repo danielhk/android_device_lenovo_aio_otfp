0416241d92184133a57305881ab72e265dd79e30
daniel_hk <daniel.p6800@gmail.com>
2016-12-05 17:43:35 +0800

    Add new flags for Bluetooth

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,16 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Lenovo K3-Note"
+#define BTM_DEF_LOCAL_NAME   "Lenovo K3 Note"
+
+// Networking, Capturing, Object Transfer
+// MAJOR CLASS: COMPUTER
+// MINOR CLASS: PALM SIZE PC/PDA
+#define BTA_DM_COD {0x1A, 0x01, 0x14}
+
+//#define BTIF_HF_SERVICES (BTA_HSP_SERVICE_MASK)
+//#define BTIF_HF_SERVICE_NAMES  { BTIF_HSAG_SERVICE_NAME, NULL }
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+//#define PAN_NAP_DISABLED TRUE
 
 #endif
