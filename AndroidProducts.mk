8c86138f311ee68b480ae8c25cb2e1283d5e4470
danielhk <daniel_hk>
2017-08-16 14:49:11 +0800

    Remove unused makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,3 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/carbon_aio_otfp.mk \
 	$(LOCAL_DIR)/cm.mk \
 	$(LOCAL_DIR)/full_aio_otfp.mk \
-	$(LOCAL_DIR)/omni_aio_otfp.mk
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo carbon_aio_otfp-userdebug
-add_lunch_combo omni_aio_otfp-userdebug
-
