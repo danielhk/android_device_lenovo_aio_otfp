2847c6f520e25ed5b64cb5cd6e63c69fdee72c9d
danielhk <daniel_hk>
2017-10-07 05:03:55 +0800

    Add Health HAL implementation for Oero

--- device.mk
+++ device.mk
@@ -166,6 +166,11 @@ PRODUCT_PACKAGES += \
     libgralloc_extra \
     libgui_ext
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
