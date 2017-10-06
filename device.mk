9069dd67440beed768b146db4d0063fbf88a182e
danielhk <daniel_hk>
2017-10-07 05:03:07 +0800

    Add Graphic HAL implementation for Oero

--- device.mk
+++ device.mk
@@ -157,6 +157,15 @@ PRODUCT_PACKAGES += \
     libcurl \
     YGPS
 
+# Graphic HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    libgralloc_extra \
+    libgui_ext
+
 # WiFi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
