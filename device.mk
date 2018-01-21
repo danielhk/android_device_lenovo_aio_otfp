14b5fd82c88e69ac1c9fd3c7254ef4a5a499b4bb
danielhk <daniel.p6800@gmail.com>
2018-01-21 14:52:08 +0800

    Add Vibrator HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -196,6 +196,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
